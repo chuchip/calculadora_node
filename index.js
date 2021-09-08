1c347c2123cd4809a55acbcc80ba5419b6db66c6
Jesus Javier Puente <chuchip@gmail.com>
2021-09-08 06:44:11 +0200

    Avanzando con formularios

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // load the things we need
 var express = require('express');
 var app = express();
-
+var tiempo=0;
+var datos=[];
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+app.use( express.json());
 // use res.render to load up an ejs view file
 
 // index page 
@@ -22,10 +25,42 @@ app.get('/', function(req, res) {
     });
 });
 
+app.get('/formulario', (req, res) => {
+    res.render('pages/formulario');
+});
+
+app.post('/accion', (req, res) => {
+    const accion=req.body.accion;
+    const numero=parseFloat(req.body.numero);
+    console.log(`Accion ${accion} Numero: ${numero}`);
+    datos.push({accion,numero,fecha: new Date()});
+    console.log(datos);
+    res.render('pages/accion', 
+    {
+        log: JSON.stringify(req.body)
+    });
+}); 
+app.post('/api', (req, res) => {
+    console.log(req.body);
+    res.render('pages/accion', 
+    {
+        log: JSON.stringify(req.body)
+    });
+});
 // about page
-app.get('/about', function(req, res) {
+app.get('/about', (req, res) => {
     res.render('pages/about');
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+app.listen(8080, ()=>
+ {
+      console.log('8080 is the magic port')
+      setInterval(incrementaTiempo, 1000);
+ });
+
+function incrementaTiempo()
+{
+    tiempo++;
+    //console.log("Tiempo pasado: "+tiempo);
+    
+}
